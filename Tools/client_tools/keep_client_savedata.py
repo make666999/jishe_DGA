8a08de39c71891edc6a27cc0c73d616b8295b7f0
make666999 <1274171989@qq.com>
2024-01-22 20:15:54 +0800

    变更

--- Tools/client_tools/keep_client_savedata.py
+++ Tools/client_tools/keep_client_savedata.py
@@ -1,11 +1,16 @@
 import socket
-
+from queue import Queue
 import psutil
 import time
 from Tools.client_tools import get_loc_ip
 from Tools.mongo_tools import mongo_link
+import threading
+from concurrent.futures import ThreadPoolExecutor
 # 记录上一次的连接信息
-def get_doamin(ip):
+content_queue = Queue()
+db_now = mongo_link.mongo_link_database("DGA_IP_now")
+db_log = mongo_link.mongo_link_database("DGA_IP_log")
+def get_domain(ip):
     try:
         # Get the hostname from the IP address
         hostname = socket.gethostbyaddr(ip)[0]
@@ -13,18 +18,33 @@ def get_doamin(ip):
     except socket.herror:
         # Return the original IP address if no hostname is found
         return ip
+def process_connection(conn):
+    remote_addr = f'{conn.raddr.ip}:{conn.raddr.port}'
+    status = conn.status
+    domain = get_domain(conn.raddr.ip)
+    if domain != conn.raddr.ip:
+        data = {
+            "remote_address": remote_addr,
+            "remote_domain": domain,
+            "status": status,
+            "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        }
+        content_queue.put(data)
 
 def keep_client_save_ip_now_log():
+
     db_now = mongo_link.mongo_link_database("DGA_IP_now")
     db_log = mongo_link.mongo_link_database("DGA_IP_log")
     db_now.delete_many({})
     db_log.delete_many({})
     last_connections = set()
+    executor = ThreadPoolExecutor(max_workers=10)
     while True:
         # 获取当前的连接信息
         connections = set(get_loc_ip.select_conn_ipv4())
         # 找到新建立的连接
         new_connections = list(connections - last_connections)
+
         # 找到关闭的连接
         closed_connections = list(last_connections - connections)
 
@@ -33,28 +53,20 @@ def keep_client_save_ip_now_log():
             new_data = []
             print("发现新连接：", len(new_connections))
             for conn in new_connections:
-                local_addr = f'{conn.laddr.ip}:{conn.laddr.port}'
-                remote_addr = f'{conn.raddr.ip}:{conn.raddr.port}'
-                status = conn.status
-                data = {
-                    "local_address": local_addr,
-                    "remote_address": remote_addr,
-                    "status": status,
-                    "timestamp":(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))  # 添加一个时间戳字段
-                }
-                new_data.append(data)
-
-            db_now.insert_many(new_data)
-            db_log.insert_many(new_data)
+                executor.submit(process_connection, conn)
+            while not content_queue.empty():
+                item = content_queue.get()
+                db_now.insert_one(item)
+                db_log.insert_one(item)
 
         # 处理关闭的连接
         if closed_connections:
             delete_data = []
             print("连接已关闭：", len(closed_connections))
             for conn in closed_connections:
-                local_addr = f'{conn.laddr.ip}:{conn.laddr.port}'
+
                 remote_addr = f'{conn.raddr.ip}:{conn.raddr.port}'
-                delete_data.append({"local_address": local_addr, "remote_address": remote_addr})
+                delete_data.append({ "remote_address": remote_addr})
             db_now.delete_many({"$or": delete_data})
 
         # 更新上一次的连接信息
