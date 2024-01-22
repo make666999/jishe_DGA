7ef49a243f23bf8789b25c0029b51a10ba468d24
make666999 <1274171989@qq.com>
2024-01-22 14:31:52 +0800

    变更

---
+++ Tools/mongo_tools/mongo_link.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+# 创建 MongoClient 对象，指定 MongoDB 的连接地址和端口号
+def mongo_link_database(database_name):
+    client = MongoClient("mongodb://8c630x9121.goho.co:23593")
+
+    # 选择数据库
+    db = client["test"]
+    # 选择集合
+    collection = db[database_name]
+    return collection
+
+
