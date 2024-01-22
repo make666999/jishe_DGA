5a1c50f85812e5ecf11099fac9c34619a6693bd4
make666999 <1274171989@qq.com>
2024-01-22 15:57:49 +0800

    变更

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from Tools.mongo_tools import mongo_link
 
 if __name__ == '__main__':
-    db= mongo_link.mongo_link_database("DGA")
-    res=db.insert_one({"URL": "www,baidu.com", "type": "True"})
+    ##获取主机网络连接
+
+    ##存入数据库
+
+    db = mongo_link.mongo_link_database("DGA")
+    res = db.insert_one({"URL": "www,baidu.com", "type": "True"})
     print("插入成功，文档ID：", res.inserted_id)
+
     documents = db.find()
     # 打印每个文档
     for doc in documents:
         print(doc)
-
