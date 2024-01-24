1b7c81ca274c93fe5171f3554b9271cd4910c42f
tabuaihua <131175343+tabuaihua@users.noreply.github.com>
2024-01-24 22:36:25 +0800

    更改路径

--- Tools/database_tools/dababase_use.py
+++ Tools/database_tools/dababase_use.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import geoip2.database
 # 创建 MongoClient 对象，指定 MongoDB 的数据库、集合
 
-# reader_path = os.path.abspath(os.path.join(".","Model", "Data","city_db", "GeoLite2-City.mmdb"))
+# reader_path = os.path.abspath(os.path.join("Model", "Data","city_db", "GeoLite2-City.mmdb"))
 reader_path="./Model/Data/city_db/GeoLite2-City.mmdb"
 reader=geoip2.database.Reader(reader_path)
 def mongo_link_database(database_name):
