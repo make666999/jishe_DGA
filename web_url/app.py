ec1fdc2201f9516e5a31140674b40920dc6a9534
tabuaihua <131175343+tabuaihua@users.noreply.github.com>
2024-01-25 19:42:25 +0800

    web_路径映射图

--- web_url/app.py
+++ web_url/app.py
@@ -4,24 +4,27 @@ from fastapi.websockets import WebSocketState
 
 from fastapi import FastAPI, WebSocket, Request
 from pymongo import MongoClient
-
+import pymongo
 from fastapi import FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from bson.json_util import dumps
 import asyncio
 
+from starlette.responses import JSONResponse
+
 app = FastAPI()
 
 # 连接到MongoDB数据库
-
-
 mongo_client = MongoClient("mongodb://8c630x9121.goho.co:23593")
 db = mongo_client["DGA"]
-collection = db["DGA_Domain_Log"]
+collection = db["tabuaihua"]
 # 模板配置
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+
 
 
 async def get_latest_data():
@@ -29,7 +32,31 @@ async def get_latest_data():
     latest_data = list(collection.find().sort("Timestamp", -1).limit(10))
     return latest_data
 
+async def get_city_data():
+    query = {"Loc_Data": {"$ne": None}}
+    projection = {"_id": 0,"Loc_Data": 1}
 
+    city_data = collection.find(query,projection)
+    city_data = [document["Loc_Data"] for document in city_data]
+
+    return city_data
+
+
+
+
+
+@app.websocket("/city_map")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while websocket.client_state == WebSocketState.CONNECTED:
+            City_Data = await get_city_data()
+            print("City Data:", City_Data)
+            await websocket.send_text(dumps(City_Data))
+            await asyncio.sleep(1)
+    finally:
+        # 关闭连接时的清理工作
+        await websocket.close()
 
 
 @app.websocket("/ws")
@@ -41,6 +68,8 @@ async def websocket_endpoint(websocket: WebSocket):
             print("Latest Data:", latest_data)
             await websocket.send_text(dumps(latest_data))
             await asyncio.sleep(1)
+    except:
+        print("Error")
     finally:
         # 关闭连接时的清理工作
         await websocket.close()
@@ -51,3 +80,12 @@ async def read_root(request: Request):
     latest_data = await get_latest_data()
     return templates.TemplateResponse("index.html", {"request": request, "latest_data": latest_data})
 
+
+
+
+
+@app.get("/data_ui")
+async def read_root(request: Request):
+    city_data = await get_city_data()
+    print(city_data)
+    return templates.TemplateResponse("data_ui.html", {"request": request, "allDataParam": city_data})
