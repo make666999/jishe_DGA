9a5aa007fedc092c68f19a154274c9b2d04bc884
make666999 <1274171989@qq.com>
2024-01-26 16:14:22 +0800

    恢复

--- web_url/templates/static/global.js
+++ web_url/templates/static/global.js
@@ -1,3 +1,3 @@
 const ipAddress = '192.168.78.49:8000';
-const socket_city_map = new WebSocket(`ws://${ipAddress}/`);
+const socket_city_map = new WebSocket(`ws://${ipAddress}/city_map`);
 const socket_ws = new WebSocket(`ws://${ipAddress}/ws`);
