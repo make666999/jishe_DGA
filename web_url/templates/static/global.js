8c2c61775e0fbf6b7a3a4fc1a6dbca8c0dae871f
elaine171717 <elaine171717@163.com>
2024-02-19 23:59:20 +0800

    2.19修改首页，状态，表格

--- web_url/templates/static/global.js
+++ web_url/templates/static/global.js
@@ -1,4 +1,4 @@
-const ipAddress = '192.168.78.49:8000';
+const ipAddress = '192.168.1.47:8000';
 
 // const socket_city_map = new WebSocket(`ws://${ipAddress}/city_map`);
 // const socket_ws = new WebSocket(`ws://${ipAddress}/ws`);
--- web_url/templates/test.js
+++ web_url/templates/test.js
@@ -1,74 +1,29 @@
-/* test.js */
-var data = [
-{ y: '2014', a: 50, b: 90 },
-{ y: '2015', a: 65, b: 75 },
-{ y: '2016', a: 50, b: 50 },
-{ y: '2017', a: 75, b: 60 },
-{ y: '2018', a: 80, b: 65 },
-{ y: '2019', a: 90, b: 70 },
-{ y: '2020', a: 100, b: 75 },
-{ y: '2021', a: 115, b: 75 },
-{ y: '2022', a: 120, b: 85 },
-{ y: '2023', a: 145, b: 85 },
-{ y: '2024', a: 160, b: 95 }
-];
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
 
-var areaConfig = {
-element: 'area-chart',
-data: data,
-xkey: 'y',
-ykeys: ['a', 'b'],
-labels: ['Total Income', 'Total Outcome'],
-fillOpacity: 0.6,
-hideHover: 'auto',
-behaveLikeLine: true,
-resize: true,
-pointFillColors: ['#ffffff'],
-pointStrokeColors: ['black'],
-lineColors: ['gray', 'red']
-};
+    if(h == 0){
+        h = 12;
+    }
 
-var lineConfig = {
-element: 'line-chart',
-data: data,
-xkey: 'y',
-ykeys: ['a', 'b'],
-labels: ['Total Income', 'Total Outcome'],
-lineColors: ['blue', 'green']
-};
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
 
-var barConfig = {
-element: 'bar-chart',
-data: data,
-xkey: 'y',
-ykeys: ['a', 'b'],
-labels: ['Total Income', 'Total Outcome'],
-barColors: ['#f0ad4e', '#d9534f']
-};
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
-var stackedConfig = {
-element: 'stacked',
-data: data,
-xkey: 'y',
-ykeys: ['a', 'b'],
-labels: ['Total Income', 'Total Outcome'],
-stacked: true
-};
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
 
-var pieConfig = {
-element: 'pie-chart',
-data: [
-{ label: "Friends", value: 30 },
-{ label: "Allies", value: 15 },
-{ label: "Enemies", value: 45 },
-{ label: "Neutral", value: 10 }
-],
-colors: ['#428bca', '#5bc0de', '#d9534f', '#5cb85c']
-};
+    setTimeout(showTime, 1000);
 
+}
 
-Morris.Area(areaConfig);
-Morris.Line(lineConfig);
-Morris.Bar(barConfig);
-Morris.Bar(stackedConfig);
-Morris.Donut(pieConfig);
+showTime();
